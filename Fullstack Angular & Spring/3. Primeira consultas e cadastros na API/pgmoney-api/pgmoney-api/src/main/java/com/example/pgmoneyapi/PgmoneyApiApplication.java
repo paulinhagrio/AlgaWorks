c11d6d4ccc5c6f77a68baba325676b4d0cfeaef6
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-09-29 15:58:12 -0300

    3. Primeira consultas e cadastros na API

---
+++ Fullstack Angular & Spring/3. Primeira consultas e cadastros na API/pgmoney-api/pgmoney-api/src/main/java/com/example/pgmoneyapi/PgmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pgmoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgmoneyApiApplication.class, args);
+	}
+
+}
---
+++ Fullstack Angular & Spring/3. Primeira consultas e cadastros na API/pgmoney-api/pgmoney-api/src/test/java/com/example/pgmoneyapi/PgmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pgmoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
