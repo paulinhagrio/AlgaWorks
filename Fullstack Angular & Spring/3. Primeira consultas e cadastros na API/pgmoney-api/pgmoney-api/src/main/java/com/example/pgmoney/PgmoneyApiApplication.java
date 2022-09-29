c4472f1285900f2ea4bc107ef57fc194155f785a
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-09-29 16:34:07 -0300

    Criacao do Banco de Dados

--- Fullstack Angular & Spring/3. Primeira consultas e cadastros na API/pgmoney-api/pgmoney-api/src/main/java/com/example/pgmoney/PgmoneyApiApplication.java
+++ Fullstack Angular & Spring/3. Primeira consultas e cadastros na API/pgmoney-api/pgmoney-api/src/main/java/com/example/pgmoney/PgmoneyApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.pgmoneyapi;
+package com.example.pgmoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
