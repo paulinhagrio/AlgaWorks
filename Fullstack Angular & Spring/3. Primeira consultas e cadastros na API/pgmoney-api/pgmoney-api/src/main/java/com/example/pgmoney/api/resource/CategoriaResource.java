1cdfd4d471dbe8ce883a30dcd2e4a4963ddbb768
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-09-29 21:19:14 -0300

    lição 3.6._Criação da primeira Categoria e do método findById.

--- Fullstack Angular & Spring/3. Primeira consultas e cadastros na API/pgmoney-api/pgmoney-api/src/main/java/com/example/pgmoney/api/resource/CategoriaResource.java
+++ Fullstack Angular & Spring/3. Primeira consultas e cadastros na API/pgmoney-api/pgmoney-api/src/main/java/com/example/pgmoney/api/resource/CategoriaResource.java
@@ -3,11 +3,14 @@ package com.example.pgmoney.api.resource;
 import com.example.pgmoney.api.model.Categoria;
 import com.example.pgmoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -15,11 +18,26 @@ public class CategoriaResource {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
-    
+
     @GetMapping
     public List<Categoria> listar(){
         return  categoriaRepository.findAll();
+    }
 
+    @PostMapping
+    public ResponseEntity<Categoria> criar( @RequestBody Categoria categoria, HttpServletResponse response){
+       Categoria categoriaSalva = categoriaRepository.save(categoria);
 
+       URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+               .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+       response.setHeader("Location", uri.toASCIIString());
+
+       return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo){
+        return categoriaRepository.findById(codigo);
     }
 }
